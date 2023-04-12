2e07b597a675d2dcda15f4007d75dda101153b1d
Vidita Kumari <kvidita@users.noreply.github.com>
2023-04-12 19:37:22 +0530

    made the function for finding max index of the given array. All test passing.

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,6 +1,11 @@
+const arrangeArray = function() {
+
+}
+
 const dispenseCoins = function(denominations, amount) {
 	let coinCount = 0;
 	let amountLeft = amount;
+
 	for (let count = denominations.length - 1; count >= 0; count--) {
 		coinCount = coinCount + Math.floor(amountLeft/denominations[count]);
 		amountLeft = amountLeft % denominations[count];
@@ -10,3 +15,15 @@ const dispenseCoins = function(denominations, amount) {
 
 exports.dispenseCoins = dispenseCoins;
 
+const indexOfMax = function(array) {
+	let maxIndex = -1;
+	for(let count = 1; count < array.length; count++) {
+		if (array[count] > array[count - 1]){
+			maxIndex = count ;	
+		}
+	}
+	return maxIndex;
+}
+
+
+exports.indexOfMax = indexOfMax;
